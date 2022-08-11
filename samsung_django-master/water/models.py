2092ad6a71e422e11835b56655ee30caf2d3d765
Aisha <107345910+Aishagurt@users.noreply.github.com>
2022-08-11 18:28:19 +0600

    Update models.py

--- samsung_django-master/water/models.py
+++ samsung_django-master/water/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class CalorieTracker(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    calories = models.PositiveIntegerField('Amount of calories')
+
+    def __str__(self):
+        return str(self.user)
