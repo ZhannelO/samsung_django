a281f811b437a3c8e50f55622a1b137c44d9f0c8
zhannel <98833988+ZhannelO@users.noreply.github.com>
2022-08-13 17:39:23 +0600

    Delete samsung_django-master directory

--- samsung_django-master/templates/personal/script.js
+++
@@ -1,5 +0,0 @@
-var f_asleep_time;
-var getting_up;
-f_asleep_time= Number(document.getElementById("f_asleep_time").value);
-getting_up= Number(document.getElementById("getting_up").value);
-
--- samsung_django-master/water/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class CalorieTracker(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    calories = models.PositiveIntegerField('Amount of calories')
-
-    def __str__(self):
-        return str(self.user)
