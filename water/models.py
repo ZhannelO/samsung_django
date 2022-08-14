80f85216b03218478d59f571721c54076c0452ed
Aisha <107345910+Aishagurt@users.noreply.github.com>
2022-08-14 17:47:01 +0600

    I FREACKING DID IT

--- water/models.py
+++ water/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime, date
 
 class CalorieTracker(models.Model):
     username = models.CharField('Enter username', max_length=50)
     calories = models.PositiveIntegerField('Amount of calories')
-
+    datetime = models.DateField(auto_now_add=False, auto_now=False, blank=True, null = True)
     def __str__(self):
         return self.username
