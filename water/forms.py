017c0a0687aa96d79b278573232ef355237b6829
Aisha <107345910+Aishagurt@users.noreply.github.com>
2022-08-15 00:02:51 +0600

    Update forms.py

--- water/forms.py
+++ water/forms.py
@@ -5,7 +5,7 @@ from django.forms import ModelForm, TextInput, DateInput
 class CaloriesTrackerform(ModelForm):
     class Meta:
         model = CalorieTracker
-        fields = ['user', "calories", 'datetime']
+        fields = ['username', "calories", 'datetime']
         widgets = {
             'calories': TextInput(attrs={
                 'class': 'form-control',
