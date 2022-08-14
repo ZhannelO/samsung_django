83b50e3c8c21c4e40a2af36fca5df42ef781ebd6
Aisha <107345910+Aishagurt@users.noreply.github.com>
2022-08-15 00:00:18 +0600

    Update views.py

--- water/views.py
+++ water/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.contrib import messages
 # Create your views here.
